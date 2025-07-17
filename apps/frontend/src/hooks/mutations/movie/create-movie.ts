ae258ddc53fcf243ffc8dd36a4398f984b351be3
Carlos Silva <miguel21.melo21@gmail.com>
2025-07-17 18:36:39 -0300

    feat(frontend): adds query invalidation at create

--- apps/frontend/src/hooks/mutations/movie/create-movie.ts
+++ apps/frontend/src/hooks/mutations/movie/create-movie.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 import { createMovie } from "@/services/movie";
@@ -10,10 +10,15 @@ type UseCreateMovieProps = {
 export function useCreateMovie({
   onDone = () => {},
 }: UseCreateMovieProps = {}) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createMovie,
     onSuccess() {
       toast.success("Filme adicionado com sucesso!");
+      queryClient.invalidateQueries({
+        queryKey: ["movies"],
+      });
       onDone();
     },
   });
