b6626fc39dc05d79b9fd12b9bd003127fe70a2c6
Carlos Silva <miguel21.melo21@gmail.com>
2025-07-17 18:36:51 -0300

    feat(frontend): adds movie cards

---
+++ apps/frontend/src/routes/(app)/@components/movie-card.tsx
@@ -0,0 +1,44 @@
+type MovieCardProps = {
+  coverImage: string;
+  title: string;
+  genre?: string[];
+};
+
+export function MovieCard({ coverImage, title, genre }: MovieCardProps) {
+  return (
+    <div className="hover:scale-[101%] transition-[scale] cursor-pointer">
+      <div className="relative h-full">
+        <div className="relative min-w-[255px] w-full min-h-[355px] h-full">
+          <img
+            className="w-full h-full object-cover rounded-xs shadow"
+            src={coverImage}
+            alt={title}
+          />
+          <div
+            className="pointer-events-none absolute inset-0 rounded-xs"
+            style={{
+              background:
+                "linear-gradient(to top, rgba(0,0,0,0.7) 20%, rgba(0,0,0,0.0) 50%)",
+            }}
+          />
+        </div>
+        <div className="absolute left-4 bottom-4 space-y-2">
+          <p className="font-medium uppercase text-primary-foreground">
+            {title}
+          </p>
+          {genre && (
+            <p className="text-sm font-light text-primary-foreground/60">
+              {genre.join(", ")}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function MovieCardPlaceholder() {
+  return (
+    <div className="min-h-[355px] h-full min-w-[255px] w-full bg-accent rounded-xs animate-pulse shadow" />
+  );
+}
--- apps/frontend/src/routes/(app)/_layout/index.tsx
+++ apps/frontend/src/routes/(app)/_layout/index.tsx
@@ -1,14 +1,36 @@
 import { createFileRoute } from "@tanstack/react-router";
+
 import { Filters } from "../@components/filters";
+import { useListMovies } from "@/hooks/queries/movie/use-movies";
+import { MovieCard, MovieCardPlaceholder } from "../@components/movie-card";
 
 export const Route = createFileRoute("/(app)/_layout/")({
   component: AppHomePage,
 });
 
 function AppHomePage() {
+  const { data: movies, isPending: isMoviesPending } = useListMovies();
+
   return (
-    <div className="p-6">
+    <div className="flex flex-col gap-6 p-6 h-full">
       <Filters />
+
+      <section className="flex-1 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 rounded-xs bg-secondary/70 backdrop-blur-sm p-6">
+        {isMoviesPending &&
+          Array.from({ length: 10 }).map((_, idx) => (
+            <MovieCardPlaceholder key={idx} />
+          ))}
+        {movies?.data.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            coverImage={movie.coverImage}
+            title={movie.title}
+            genre={movie.genre}
+          />
+        ))}
+      </section>
+
+      <footer className="">pagination</footer>
     </div>
   );
 }
